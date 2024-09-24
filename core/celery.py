efb128438af153bf3ae9cedf63a17033f17503e1
alex <alex@alex-Mac-mini.local>
2024-09-24 17:32:50 +0800

    Add dockerfile

---
+++ core/celery.py
@@ -0,0 +1,21 @@
+import os
+
+from celery import Celery
+
+
+# set the default Django settings module for the 'celery' app.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery("core")
+
+# read config from Django settings, the CELERY namespace would make celery
+# config keys has `CELERY` prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# discover and load tasks.py from from all registered Django apps
+app.autodiscover_tasks()
+
+
+@app.task
+def task():
+    pass
